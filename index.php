99728a7d85e68e97c650d9854a7ee8c719429637
Bryson Banks <brysonbanks04@gmail.com>
2025-09-21 20:55:16 -0500

    yes

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	echo "<h1>Welcome to My Website</h1>";
+	echo "<p>My name is Bryson banks, I have attended 2 classes that had to do with mysql and html. I been at dallas college for 1 year, I got 1 certificate last semester and have met 1 friend at the college.</p>";
+	
+?>
